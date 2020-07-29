c9a49fff473362b42886894029f70f5080b5f050
DesaiSwara <68277364+DesaiSwara@users.noreply.github.com>
2020-07-29 20:34:09 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -19,7 +19,20 @@ function setup() {
 
 function draw() {
   background(0); 
-  hasCollided();
+ 
+  if (hasCollided(bullet,wall)) {
+    bullet.velocityX=0;
+    var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
+  }
+  
+  if (damage>10) {
+    wall.shapeColor=color(255,0,0);
+  }
+  
+  if (damage<10) {
+    wall.shapeColor=color(0,255,0);
+  }
+
   drawSprites();
 }
 
@@ -27,21 +40,8 @@ function hasCollided (lbullet,lwall) {
   bulletRightEdge=lbullet.x + lbullet.width;
   wallLeftEdge=lwall.x;
 
-  if (bulletRightEdge >= wallLeftEdge) {
+  if (bulletRightEdge>=wallLeftEdge) {
     return true;
   }
   return false;
-}
-
-if (hasCollided) {
-  bullet.velocityX=0;
-  var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
-}
-
-if (damage > 10) {
-  wall.shapeColor=color(255,0,0);
-}
-
-if (damage < 10) {
-  wall.shapeColor=color(0,255,0);
 }
